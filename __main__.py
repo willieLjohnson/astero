406a9d2677901717867181fb12aea74657a9bc04
Willie Johnson <liwa.johnson@gmail.com>
2021-12-26 20:55:52 -0500

    init basic pygame project

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from game import Astero
+
+if __name__ == "__main__":
+    astero = Astero()
+    astero.main_loop()
---
+++ game.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Astero:
+    def __init__(self):
+        self.__init_pygame()
+        self.screen = pygame.display.set_mode((800, 600))
+
+    def __init_pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Astero")
+
+    def main_loop(self):
+        while True:
+            self._handle_input()
+            self._process_game_logic()
+            self._draw()
+
+    def _handle_input(self):
+        pass
+
+    def _process_game_logic(self):
+        pass
+
+    def _draw(self):
+        self.screen.fill((0, 0, 255))
+        pygame.display.flip()
+ 
