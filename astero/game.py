0b92bb1536c441c7dd42b8b3a40f18421090b3e4
Willie Johnson <liwa.johnson@gmail.com>
2021-12-28 13:29:05 -0500

    add sprites to player and steroid

--- astero/game.py
+++ astero/game.py
@@ -1,12 +1,15 @@
 import pygame
 
-from utils import load_sprite
+from models import GameObject
+from utils import load_sprite, load_sprite_from_sheet
 
 class Astero:
     def __init__(self):
         self.__init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
         self.background = load_sprite("background", "jpg", False)
+        self.spaceship = GameObject((400, 300), load_sprite_from_sheet("spaceship", (40, 0, 40, 40)), (0, 0))
+        self.asteroid = GameObject((400, 300), load_sprite_from_sheet("rock", (0, 0, 64, 64)), (1, 0))
 
     def __init_pygame(self):
         pygame.init()
@@ -26,9 +29,12 @@ class Astero:
                 quit()
 
     def _process_game_logic(self):
-        pass
+        self.spaceship.move()
+        self.asteroid.move()
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
+        self.spaceship.draw(self.screen)
+        self.asteroid.draw(self.screen)
         pygame.display.flip()
  
