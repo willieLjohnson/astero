aa77a3df9ba52043f5978f95be2f1819a7c9ab75
Willie Johnson <liwa.johnson@gmail.com>
2021-12-28 14:20:06 -0500

    update sprite shape and rotation for spaceship

--- astero/models.py
+++ astero/models.py
@@ -1,6 +1,12 @@
 from pygame.math import Vector2
+from pygame.transform import rotozoom
 
 from utils import load_sprite_from_sheet
+
+UP = Vector2(0, -1)
+DOWN = Vector2(0, 1)
+LEFT = Vector2(-1, 0)
+RIGHT = Vector2(1, 0)
 
 class GameObject:
     def __init__(self, position, sprite, velocity):
@@ -21,5 +27,19 @@ class GameObject:
         return distance < self.radius + other.radius
  
 class Spaceship(GameObject):
+    MANEUVERABILITY = 3
     def __init__(self, position):
+        self.direction = Vector2(UP)
         super().__init__(position, load_sprite_from_sheet("spaceship", (40, 0, 40, 40)), Vector2(0))
+    
+    def rotate(self, clockwise=True):
+        sign = 1 if clockwise else -1
+        angle = self.MANEUVERABILITY * sign
+        self.direction.rotate_ip(angle)
+
+    def draw(self, surface):
+        angle = self.direction.angle_to(UP)
+        rotated_surface = rotozoom(self.sprite, angle, 1.0)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
